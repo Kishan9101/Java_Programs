77d705cd6e685d763bf07137009934bccca617fe
Kishan9101 <77090002+Kishan9101@users.noreply.github.com>
2021-01-19 19:27:20 +0530

    Create Hello_World.java

---
+++ Hello_World.java
@@ -0,0 +1,12 @@
+package abcd;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hello World");
+		System.out.println("Welcome To Java");// TODO Auto-generated method stub
+
+	}
+
+}
